c63ecb31854924b2136a41b63db67819f8bcdd04
Francisco Escher <egs.francisco@gmail.com>
2019-01-06 01:25:57 -0200

    gets dependency from local repository and adds zecusdt currency pair

--- xchange-core/src/main/java/org/knowm/xchange/currency/CurrencyPair.java
+++ xchange-core/src/main/java/org/knowm/xchange/currency/CurrencyPair.java
@@ -337,9 +337,9 @@ public class CurrencyPair implements Comparable<CurrencyPair>, Serializable {
   public static final CurrencyPair OMG_USDT = new CurrencyPair(Currency.OMG, Currency.USDT);
   public static final CurrencyPair ETC_USDT = new CurrencyPair(Currency.ETC, Currency.USDT);
   public static final CurrencyPair QTUM_USDT = new CurrencyPair(Currency.QTUM, Currency.USDT);
-    public static final CurrencyPair ZEC_USDT = new CurrencyPair(Currency.ZEC, Currency.USDT);
+  public static final CurrencyPair ZEC_USDT = new CurrencyPair(Currency.ZEC, Currency.USDT);
 
-    public final Currency base;
+  public final Currency base;
   public final Currency counter;
 
   /**
